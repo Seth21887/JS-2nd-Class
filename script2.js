a87889f696daff203c7ec50a067ede1cd991b682
Seth21887 <101611145+Seth21887@users.noreply.github.com>
2022-04-06 15:30:21 -0700

    assignment 2

--- script2.js
+++ script2.js
@@ -12,9 +12,22 @@ console.log("Subtotal: " + subtotal);
 console.log("Taxes: " + purchaseTaxes);
 console.log("Total: " + total);
 
-document.write("Customer Receipt");
-document.write("Purchase: " + productName + " x " + quantity);
-document.write("Price per unit: " + productPrice);
-document.write("Subtotal: " + subtotal);
-document.write("Taxes: " + purchaseTaxes);
-document.write("Total: " + total);
+// did this section wrong using document.write for every single line, made it a hassle and was difficult to create a div this way.
+
+// document.write(`<h3>Customer Receipt</h3>`);
+// document.write(`<b>Purchase:</b> ` + productName + ` x ` + quantity + "<br>");
+// document.write("<b>Price per unit:</b> " + productPrice + "<br>");
+// document.write("<b>Subtotal:</b> " + subtotal + "<br>");
+// document.write("<b>Taxes:</b> " + purchaseTaxes + "<br>");
+// document.write("<b>Total:</b> " + total + "<br>");
+
+document.write(`
+<div class=receipt>
+    <h3>Customer Receipt</h3>
+    <p><b>Purchase: </b>${productName} x ${quantity}</p>
+    <p><b>Price Per Unit: </b>${productPrice}</p>
+    <p><b>Subtotal: </b>${subtotal}</p>
+    <p><b>Taxes: </b>${taxes}</p>
+    <p><b>Total: </b>${total}</p>
+
+`)
